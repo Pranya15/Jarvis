cd3164a67a697fa4bc464cce4af3569ba522626a
Pranya Patel <patelpranya15@gmail.com>
2026-04-26 17:35:25 +0530

    added

---
+++ musiclibrary.py
@@ -0,0 +1,9 @@
+music = {
+   "doorondooron": "https://www.youtube.com/watch?v=9T-Zbxg9X_4&list=RD9T-Zbxg9X_4&start_radio=1",
+   "tumsehi": "https://www.youtube.com/watch?v=Cb6wuzOurPc&list=RDCb6wuzOurPc&start_radio=1",
+   "nightchanges": "https://www.youtube.com/watch?v=syFZfO_wfMQ&list=RDsyFZfO_wfMQ&start_radio=1",
+   "bairan": "https://www.youtube.com/watch?v=pPPFWcpIs38&list=RDpPPFWcpIs38&start_radio=1",
+   "baby":"https://www.youtube.com/watch?v=kffacxfA7G4&list=RDkffacxfA7G4&start_radio=1",
+   "perfect":"https://www.youtube.com/watch?v=2Vv-BfVoq4g&list=RD2Vv-BfVoq4g&start_radio=1",
+   "yellow":"https://www.youtube.com/watch?v=yKNxeF4KMsY&list=RDyKNxeF4KMsY&start_radio=1",
+}  
